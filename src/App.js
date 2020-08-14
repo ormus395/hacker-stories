2286c6dad03ba0016514d3af2f5603d2f68092fb
ormus395 <ormus395@gmail.com>
2020-08-14 10:14:00 -0700

    Moved onto React State

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Hacker Stories</h1>
+      <Search onChangeHandler={handleChange} />
+      <hr />
+      <ul>
+        <List list={stories} />
+      </ul>
     </div>
   );
-}
+};
+
+const Search = ({ onChangeHandler }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input onChange={onChangeHandler} id="search" type="text" />
+  </div>
+);
+
+const List = ({ list }) =>
+  list.map((item) => (
+    <li key={item.objectID}>
+      <h3>
+        <a href={item.url}>{item.title}</a>
+      </h3>
+      <span>{item.author} </span>
+      <span>{item.num_comments} </span>
+      <span>{item.points}</span>
+    </li>
+  ));
 
 export default App;
