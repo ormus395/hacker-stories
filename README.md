871d7d5d54dca4106fdebec63ad8dae6493a94ce
ormus395 <ormus395@gmail.com>
2020-08-15 02:31:54 -0700

    data fetching in react

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# finished reading:
+# bookmark
 
-    props handling
+    react fundamentals - Data Re-Fetching in React
 
 # Excersises todo:
 
 ### Chapter Exercises
 
-- **Chapter:** React Fundementals: Props Handling
+- **Chapter:** React Fundementals - Props Handling
 
   - [x] Read more about javascripts desctructuring assignment
         Used to break apart an object or an array
@@ -70,3 +70,39 @@
         console.log(concatArgs("Programming","is","cool"))
 
   - [x] Get a good sense about JavaScript (e.g. spread operator, rest parameters, destructuring) and what's related to React (e.g. props) from the last lessons.
+
+- **Chapter:** React Fundementals - React Side-Effects
+
+  - [x] read more about Reacts useEffect Hook
+  - [x] Give the first argument's function a console.log() and experiment with React's useEffect Hook's dependency array. Check the logs for an empty dependency array too.
+
+- **Chapter:** React Fundementals - React Custom Hooks (Advanced)
+
+  - [] Read more about React Hooks, and develop an understanding of them.
+
+- **Chapter:** React Fundementals - Fragments
+
+  - [x] learn about fragments
+        Fragments are a solution to get around wrapping top level elements in a div which could create incorrect html; React.Fragment or <></> do not render html, so tables and lists can be rendered without random divs in their markup
+
+- **Chapter:** React Fundementals - Imperative React
+
+  - [] Read more about reacts useRef Hook
+
+- **Chapter:** React Fundementals - Inline Handler in JSX
+
+  - [] Review handlers: callback handlers, and inline handlers.
+
+- **Chapter:** React Fundementals - React Conditional Rendering
+
+  - [] read more about conditional rendering in React
+
+- **Chapter:** React Fundementals - React Advanced State
+
+  - [][read more about reducers in javascript.](https://www.robinwieruch.de/javascript-reducer) **!THIS IS IMPORTANT!**
+  - [] Read more about reducers and useReducer in React **THIS IS IMPORTANT!** [1](https://www.robinwieruch.de/react-usereducer-hook)
+    [2](https://reactjs.org/docs/hooks-reference.html#usereducer)
+
+- **Chapter:** React Fundementals - React Impossible States
+  - [] Read over the previously linked tutorials about reducers in JavaScript and React.
+  - [][read more about when to use usestate or usereducer in react.](https://www.robinwieruch.de/react-usereducer-vs-usestate)
--- src/App.js
+++ src/App.js
@@ -1,75 +1,192 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-const welcome = {
-  greeting: "Hey",
-  title: "React",
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
+const storiesReducer = (state, action) => {
+  console.log(state, action);
+  switch (action.type) {
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "REMOVE_STORY":
+      return {
+        ...state,
+        data: state.data.filter((story) => action.payload !== story.objectID),
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      console.log("Something broke");
+  }
+};
+
+const useSemiPersistanceState = (key, initialState = "") => {
+  const [value, setvalue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+
+  React.useEffect(() => {
+    let random = Math.floor(Math.random() * 10);
+
+    console.log(random);
+  }, [value]);
+
+  return [value, setvalue];
 };
 
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [searchState, setSearchState] = useSemiPersistanceState(
+    "search",
+    "React"
+  );
 
-  const [searchState, setSearchState] = React.useState("");
+  const [state, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+
+  console.log(state);
+
+  React.useEffect(() => {
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
+
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        dispatchStories({
+          type: "STORIES_FETCH_SUCCESS",
+          payload: response.hits,
+        });
+      })
+      .catch((err) => {
+        dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+      });
+  }, []);
+
+  const handleRemoveStory = (objectID) => {
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: objectID,
+    });
+  };
 
   const handleSearch = (event) => {
     setSearchState(event.target.value);
   };
 
-  const searchedStories = stories.filter((story) =>
+  const searchedStories = state.data.filter((story) =>
     story.title.toLowerCase().includes(searchState.toLowerCase())
   );
 
-  console.log(searchedStories);
+  let listContent = "Loading...";
+
+  if (state.isError && !state.isLoading) {
+    listContent = <p>An Error has occured.</p>;
+  } else if (!state.isLoading && !state.isError) {
+    listContent = (
+      <ul>
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      </ul>
+    );
+  }
   return (
     <div className="App">
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch} searchValue={searchState} />
+      <InputWithLabel
+        onInputChange={handleSearch}
+        value={searchState}
+        id="search"
+        label="Search"
+        type="text"
+        isFocused={true}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
       <p>Searching for: {searchState}</p>
       <hr />
-      <ul>
-        <List list={searchedStories} />
-      </ul>
+      {listContent}
     </div>
   );
 };
 
-const Search = ({ onSearch, searchValue }) => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input onChange={onSearch} id="search" type="text" value={searchValue} />
-  </div>
-);
+const InputWithLabel = ({
+  id,
+  value,
+  onInputChange,
+  type,
+  children,
+  isFocused,
+}) => {
+  const inputRef = React.useRef();
 
-const List = ({ list }) =>
-  list.map((item) => <Item key={item.objectID} {...item} />);
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
 
-const Item = ({ url, title, author, num_comments, points }) => (
-  <li>
-    <h3>
-      <a href={url}>{title}</a>
-    </h3>
-    <span>{author} </span>
-    <span>{num_comments} </span>
-    <span>{points}</span>
-  </li>
-);
+  return (
+    <>
+      <label htmlFor={id}>{children} </label>
+      <input
+        ref={inputRef}
+        onChange={onInputChange}
+        id={id}
+        type={type}
+        value={value}
+      />
+    </>
+  );
+};
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} {...item} onRemoveItem={onRemoveItem} />
+  ));
 
+const Item = ({
+  objectID,
+  url,
+  title,
+  author,
+  num_comments,
+  points,
+  onRemoveItem,
+}) => {
+  return (
+    <li>
+      <h3>
+        <a href={url}>{title}</a>
+      </h3>
+      <span>{author} </span>
+      <span>{num_comments} </span>
+      <span>{points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(objectID)}>
+          Dismiss
+        </button>
+      </span>
+    </li>
+  );
+};
 export default App;
